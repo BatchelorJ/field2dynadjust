dccb6ba4a315533af7a8de08416f74c319dde3fe
BatchelorJ <josh.batchelor86@gmail.com>
2018-08-07 16:51:47 +1000

    Added file massager for fbk, ready for integration into classes

--- conversions.py
+++ conversions.py
@@ -1,16 +1,15 @@
 import os
 
-def stripfile(filepath, listofterms):
+def stripfile(filedata, listofterms):
     """
-    Creates a new file with lines starting with strings from a list
+    Creates a list with lines starting with strings from a list
     :param filepath: filepath
     :param listofterms: list of strings to use as search terms
-    :return: file_stage1
+    :return: list of file lines starting with strings from list of terms
     """
-    filepathin, ext = os.path.splitext(filepath)
-    filepathout = filepathin + '_stage1' + ext
-    with open(filepathout, 'w+') as f_out:
-        for line in open(filepath):
-            for i in listofterms:
-                if line.startswith(i):
-                    f_out.write(line)
+    datawithterms = []
+    for line in filedata:
+        for i in listofterms:
+            if line.startswith(i):
+                datawithterms.append(line)
+    return datawithterms
--- convertfbk.py
+++ convertfbk.py
@@ -1,16 +1,48 @@
 from conversions import stripfile
+import numpy as np
 
 testfile = '\\raw\\Site01-152.fbk'
 
 terms = ['PRISM', 'STN', 'F1', 'F2']
 
-def addprismht(filepath):
-    with open(filepath) as file:
-        for num, line in enumerate(file, 1):
-            if line.startswith('PRISM'):
-                prismht = line[6:11]
-                if line.startswith('PRISM'):
-                    break
-                num += 1
 
-    pass
+def addprismht(fbklist):
+    prismlist = []
+    rowto = []
+    numlines = 0
+    # Build array with prism height, startline and endline
+    for num, line in enumerate(fbklist, 1):
+        numlines += 1
+        if line.startswith('PRISM'):
+            prismlist.append([line[6:11], num])
+    prismarray = np.asarray(prismlist)
+    prismarrayrows = prismarray.shape[0]
+    for num, row in enumerate(prismarray, 1):
+        if num > prismarrayrows:
+            break
+        elif num == prismarrayrows:
+            rowto.append(numlines)
+        else:
+            rowto.append(prismarray[num, 1])
+    rowtoarray = np.asarray(rowto)
+    prismrange = np.append(prismarray,
+                           rowtoarray.reshape(rowtoarray.size, 1),
+                           axis=1)
+    # Add Prism Heights to each observation in file
+    filecontents = [x.strip() for x in fbklist]
+    for prism in prismrange:
+        for i in range(int(prism[1]), int(prism[2])):
+            if not (not filecontents[i].startswith('F1')
+                    and not filecontents[i].startswith('F2')):
+                filecontents[i] = filecontents[i] + ' ' + prism[0]
+    filecontents = [x + '\n' for x in filecontents]
+    return filecontents
+
+def tidyfbk(filepath):
+    with open(filepath) as f:
+        stage1 = stripfile(f, ['PRISM', 'STN', 'F1', 'F2'])
+        stage2 = addprismht(stage1)
+        stage3 = stripfile(f, ['STN', 'F1', 'F2'])
+    return stage3
+
+
